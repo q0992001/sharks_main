6675c528303d4963edbe053b8adcb39f03222aec
Tienzwuang <tienquang090901@gmail.com>
2023-09-27 16:57:23 +0700

    update data

--- script/build.js
+++ script/build.js
@@ -1,27 +1,35 @@
 let productHTML = '' ;
 
-product.forEach((product) =>  {
-  const priceInVND = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+let search = '' ;
 
-  productHTML += `
-    <div class="pro">
-      <img src="img/shoes/${product.img}" alt="">
-      <div class="des">
-        <span>Hanoi Sharks</span>
-        <h5>${product.name}</h5>
-        <div class="star">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
+product.forEach((product) =>  {
+
+  if(product.keyword.includes(search)) {
+    const priceInVND = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+
+    productHTML += `
+      <div class="pro">
+        <img src="img/shoes/${product.img}" alt="">
+        <div class="des">
+          <span>Hanoi Sharks</span>
+          <h5>${product.name}</h5>
+          <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+          <h4>${priceInVND}</h4>
         </div>
-        <h4>${priceInVND}</h4>
+        <a href="#"><img class="cart" src="cart_icon.png" alt=""></a>
       </div>
-      <a href="#"><img class="cart" src="cart_icon.png" alt=""></a>
-    </div>
-  `;
+    `;
+  }
 });
 
 document.querySelector('.js-pro-container').
+innerHTML = productHTML;
+
+document.querySelector('.js-hot-pro-container').
 innerHTML = productHTML;
